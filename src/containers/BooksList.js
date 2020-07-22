be704068408a2d7b7b8e8ae29357b29c6f10bf7a
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-22 10:58:34 -0500

    Added remove book functionality and passed it to book

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -2,25 +2,31 @@ import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Book from '../components/Book';
+import { removeBook } from '../actions';
 
-const BooksList = ({ books }) => (
-  <div className="bookslist">
-    <table>
-      <tbody>
-        {books.map(book => (
-        <div>
-          <Book book={book} key={book.id} />
-          <button id= "removeButton">Remove</button>
-        </div>
-          
-))}
+const BooksList = ({ books, removeBook }) => {
+  const handleRemoveBook = (book) => {
+    removeBook(book);
+  };
 
-      </tbody>
-    </table>
-  </div>
-);
+  return (
+    <div className="books-list">
+      <table>
+        <tbody>
+          {books.map((book) => (
+            <Book book={book} key={book.id} clickHandler={handleRemoveBook} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
 
-const mapStateToProps = state => ({ books: state.books });
+const mapStateToProps = (state) => ({ books: state.books });
+
+const mapDispatchToProps = (dispatch) => ({
+  removeBook: (book) => dispatch(removeBook(book)),
+});
 
 BooksList.defaultProps = {
   books: [],
@@ -28,7 +34,7 @@ BooksList.defaultProps = {
 
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object),
-  removeBook: PropTypes.func.isRequired
+  removeBook: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(BooksList);
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,11 +1,17 @@
 const bookReducer = (state, action) => {
   switch (action.type) {
     case 'CREATE_BOOK':
-      console.log(state);
       return { books: [...state.books, action.payload] };
 
     case 'REMOVE_BOOK':
-      return null;
+      console.log(action.payload.target);
+      return {
+        books: state.books.map((book) => {
+          if (book.id !== action.payload.id) {
+            return book;
+          }
+        }),
+      };
 
     default:
       return state;

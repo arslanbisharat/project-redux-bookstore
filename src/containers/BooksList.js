35d346a676d7d5ad1e01ee5369d67b83f1cd3d80
Luis Angel Maldonado <lmaldonadoch@gmail.com>
2020-07-22 09:47:56 -0500

    Corrected TSEs requirements

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Book from './Book';
+import Book from '../components/Book';
 
 const BooksList = ({ books }) => (
   <div className="bookslist">
     <table>
       <tbody>
-        {books.map(book => <Book book={book} key={book.id} />)}
+        {books.map((book) => (
+          <Book book={book} key={book.id} />
+        ))}
       </tbody>
     </table>
   </div>
 );
 
-const mapStateToProps = state => ({ books: state.books });
+const mapStateToProps = (state) => ({ books: state.books });
 
 BooksList.defaultProps = {
   books: [],

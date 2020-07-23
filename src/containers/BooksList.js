cd3467c428e413a11b03c6585474766f3f7b81cd
arslanbisharat <thearslan380@gmail.com>
2020-07-22 20:00:19 -0400

    Merge branch 'form-functionality' of https://github.com/arslanbisharat/project-redux-bookstore into form-functionality

--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import Book from './Book';
+import Book from '../components/Book';
 
 const BooksList = ({ books }) => (
   <div className="bookslist">
     <table>
       <tbody>
-        {books.map(book => <Book book={book} key={book.id} />)}
+        {books.map(book => (
+          <Book book={book} key={book.id} />
+        ))}
       </tbody>
     </table>
   </div>
